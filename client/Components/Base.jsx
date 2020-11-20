5e90926f50982fff8f576ede9d2a5a5c6f1ad77e
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-11-20 13:45:24 +1300

    more components

---
+++ client/Components/Base.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Base = () => {
+  return (
+    <div className='header'>
+      <h3>Pick your Base</h3>
+      <input type="radio" id="plane" name="base" value="male"/>
+      <label htmlFor="plane">plane</label>
+      <input type="radio" id="red" name="base" value="female"/>
+      <label htmlFor="red">red sause</label>
+      <input type="radio" id="yellow" name="base" value="other"/>
+      <label htmlFor="yellow">yellow sause</label>
+    </div>
+  )
+}
+
+export default Base
--- client/Components/Create.jsx
+++ client/Components/Create.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import Button2 from './Button2'
+import Cheese from './Cheese'
+import Base from './Base'
 
-const Create = () => {
-  return (
-    <>
-    <div>
-    <table>
-      <thead>
-        <tr>
-        <th><button className="button">Not a button</button></th>
-        <th><Button2 /></th>
-        </tr>
-      </thead>
-      </table>
-    
-    <img src='/images/pizza-in-an-oven-02.png'/>
-    </div>
-    </>
-  )
+class Create extends React.Component {
+  render () {
+    return (
+      <>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th><button className="button">Not a button</button></th>
+              <th><Button2 /></th>
+            </tr>
+          </thead>
+        </table>
+        <img src='/images/pizza-in-an-oven-02.png'/>
+        <Base/>
+        <Cheese />
+        <div className='header'>
+          <h3>pick your toppings</h3>
+          <input type="radio" id="meet" name="top" value="male"/>
+          <label htmlFor="meet">meet</label>
+          <input type="radio" id="things" name="top" value="female"/>
+          <label htmlFor="things">meet and thing</label>
+          <input type="radio" id="all" name="top" value="other"/>
+          <label htmlFor="all">all the things</label>
+        </div>
+      </div>
+      </>
+    )
+  }
 }
 
 export default Create
