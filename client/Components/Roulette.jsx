d154788c3180c2cc22cfbe6c7c04bacb3ec11f77
Caz <cjpitcher@gmail.com>
2020-11-20 14:06:28 +1300

    Added more weird pizza photos

--- client/Components/Roulette.jsx
+++ client/Components/Roulette.jsx
@@ -9,7 +9,7 @@ class Roulette extends React.Component {
   }
 
   clickthing= () => {
-    const ranNum = Math.floor(Math.random() * 14)
+    const ranNum = Math.floor(Math.random() * 8)
     this.setState({
       ImgLink: aName[ranNum]
     })
--- client/functions.js
+++ client/functions.js
@@ -1,2 +1,2 @@
-const aName = [ 'pizza-photo1.jpg', 'pizza-photo2.jpg', 'pizza-photo3.jpg',  ]
+const aName = [ 'pizza-photo1.jpg', 'pizza-photo2.jpg', 'pizza-photo3.jpg', 'pizza-photo4.jpg', 'pizza-photo5.jpg', 'pizza-photo6.jpg', 'pizza-photo7.jpg', 'pizza-photo8.jpg' ]
 export default aName
